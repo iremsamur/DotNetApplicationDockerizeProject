a7f2e988e01eaa860c844511aefb11a7e0ca0009
iremsmr <iremsamur129@gmail.com>
2023-01-12 11:32:04 +0300

    dotnet project dockerize example

---
+++ MyConsoleNewApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyConsoleNewApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while (i <= 1000)
+            {
+                Console.WriteLine(i + ". Merhaba Ankara");
+                i++;
+                System.Threading.Thread.Sleep(1000);//1 saniye aralıkla çalıştır
+            }
+           
+        }
+    }
+}
